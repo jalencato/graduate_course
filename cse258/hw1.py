742eea06535167c824f64dabcae4546145783e1e
jalencato <jalencato23pistons@gmail.com>
2021-09-27 21:24:22 -0700

    hw1

--- cse258/hw1.py
+++ cse258/hw1.py
@@ -1,18 +1,10 @@
-import numpy
+import numpy as np
 from urllib.request import urlopen
 import scipy.optimize
-import random
 
-def parseDataFromURL(fname):
+def parseData(fname):
     for l in urlopen(fname):
         yield eval(l)
 
-def parseData(fname):
-    for l in open(fname):
-        yield eval(l)
-
-print("Reading data...")
-# Download from http://cseweb.ucsd.edu/classes/fa19/cse258-a/data/beer_50000.json"
-data = list(parseData("data/beer/beer_50000.json"))
-# data = list(parseData("http://cseweb.ucsd.edu/classes/fa19/cse258-a/data/beer_50000.json"))
-print("done")
+# data = list(parseData('data/beer/beer_50000.json'))
+data = list(parseData(r"https://cseweb.ucsd.edu/classes/fa21/cse258-b/data/beer_50000.json"))
