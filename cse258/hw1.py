394da3444ee20cd6e07f8e1609511aef1c9dd03b
jalencato <jalencato23pistons@gmail.com>
2021-10-05 19:22:55 -0700

    hw1

--- cse258/hw1.py
+++ cse258/hw1.py
@@ -2,9 +2,24 @@ import numpy as np
 from urllib.request import urlopen
 import scipy.optimize
 
-def parseData(fname):
+
+def parseDataURL(fname):
     for l in urlopen(fname):
         yield eval(l)
 
-# data = list(parseData('data/beer/beer_50000.json'))
-data = list(parseData(r"https://cseweb.ucsd.edu/classes/fa21/cse258-b/data/beer_50000.json"))
+def parseData(fname):
+    for l in open(fname):
+        yield eval(l)
+
+print("start reading data")
+data = list(parseData('data/fantasy_10000.json'))
+# data = list(parseData(r"https://cseweb.ucsd.edu/classes/fa21/cse258-b/data/fantasy_10000.json"))
+print("review data have already been loaded")
+
+
+
+
+# f_x =
+# X = [feature(d) for d in data]
+# y = [d['review/overall'] for d in data]
+# theta, residuals, rank, s = np.linalg.lstsq(X, y)
